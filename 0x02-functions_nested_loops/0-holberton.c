21f46030cddce744340ea1cd9a7691883ffdf284
sentayehut <tsentayehu4@gmail.com>
2021-08-31 06:55:47 -0700

    0_putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
 /**
- * main - puch 0
+ * main - prints Holberton
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
-	char *c = "Holberton";
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-
-		_putchar (c[i]);
-	}
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
 }
-
