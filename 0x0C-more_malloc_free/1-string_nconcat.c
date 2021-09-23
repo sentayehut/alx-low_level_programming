65f46532d77f22f38a57093398846fea705d9b73
sentayehut <tsentayehu4@gmail.com>
2021-09-23 11:59:11 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,62 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string 1
- * @s2: string 2
- * @n: bytes to include of s2
- * Return: NULL if fail, else pointer to malloc memory
+**
+ * _strlen - return length of string
+ *
+ * @s: string to count
+ *
+ * Return: the size
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *p;
-	int strlen1, i, c;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	strlen1 = (unsigned int)_strlen(s1);
-	p = malloc((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
-	{
-		if (i < strlen1)
-			p[i] = s1[i];
-		else
-			p[i] = s2[c++];
-	}
-	p[i] = '\0';
-
-	return (p);
-}
-
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
 
 int _strlen(char *s)
 {
-	int i;
+	int counter = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: pointer to new space in memory that contains s1 + n bytes of s2
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int l1, l2, i, j;
+	char *p;
+
+	l1 = s1 != NULL ? _strlen(s1) : 0;
+	l2 = s2 != NULL ? _strlen(s2) : 0;
+
+	if (n >= l2)
+		n = l2;
+
+	p = malloc((l1 + n) * sizeof(char) + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < l1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+		p[i] = s2[j];
+
+	p[i] = 0;
+	return (p);
 }
