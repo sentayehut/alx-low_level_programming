4b4fb0d6040697b09381369732c9f450b3264cf8
sentayehut <tsentayehu4@gmail.com>
2021-09-23 09:36:55 -0700

    my main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,12 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int main(int argc, char *argv[]);
+void multiply(char *f, char *s);
+int onlyNumbers(char *c);
+int onlyNumbers(char *c);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strlen(char *s);
 #endif
