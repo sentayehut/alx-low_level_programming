7a09bc3b8f41c9cae5529b59168b586017afee96
sentayehut <tsentayehu4@gmail.com>
2021-09-23 11:33:01 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * string_nconcat - concatenate two strings
- *
- * @s1: is a pointer to the first string
- * @s2: is a pointer to the second string
- * @n: an integer (6) with the limit of what we are going to add to s1
- *
- * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	/* unsigned int var because we need to compare with n */
-	char *strings;
-	unsigned int lengths1, lengths2;
-
-	/* task condition if NULL passed, should be treated as empty string */
-	/* same condition of project 0x0B 2-str.concatena */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	/* I go through both strings so I can find their length */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
-	{
-	}
-	for (lengths2 = 0; s2[lengths2] != '\0'; lengths2++)
-	{
-	}
-	/* Now that I know the length of s2 I can compare it with n */
-	/* and set the task condition */
-	if (n > lengths2)
-		n = lengths2;
-	/* now set the malloc with the lenghts */
-	/* n is now equal to lengths2 */
-	/* plus 1 because we need to save space for the last '\0' of strings */
-	strings = malloc((lengths1 + n + 1) * sizeof(char));
-	if (strings == NULL)
-		return (NULL);
-	/* now we add the content of strings to new strings pointer */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
-		strings[lengths1] = s1[lengths1];
-	/* iterate while lengths2 < n so we print only school */
-	/* lenghts1 keeps counting while we go trough s2 */
-	for (lengths2 = 0; lengths2 < n; lengths2++, lengths1++)
-	strings[lengths1] = s2[lengths2];
-	strings[lengths1] = '\0';
-	return (strings);
+#include <stdlib.h>/**
+* string_nconcat - Concatenates two strings
+* @s1: Destination string
+* @s2: Source string
+* @n: Number of bytes to copy
+* Return: Pointer to array
+*/char *string_nconcat(char *s1, char *s2, unsigned int n)
+{	
+char *str, *str_tmp;	
+unsigned int i = 0, len_1 = 0, len_2 = 0;
+	
+s1 = s1 == NULL ? "" : s1, s2 = s2 == NULL ? "" : s2;	
+for (; s1[len_1]; ++len_1)		
+continue;	
+for (; s2[len_2] && len_2 < n; ++len_2)		
+continue;
+	
+str = malloc(sizeof(char) * (len_1 + len_2 + 1)), str_tmp = str;	
+if (str == NULL)		
+return (NULL);	
+while (*s1)		
+*str++ = *s1++;	
+for (; i < len_2; i++)		
+*str++ = *s2++;	
+*str = '\0';	
+return (str_tmp);
 }
