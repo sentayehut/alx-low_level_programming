e2cde6a580ec5ce35d466776b25b7f0fd3e5d68f
sentayehut <tsentayehu4@gmail.com>
2021-09-24 06:39:17 -0700

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,18 +5,17 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-void _puts(char *str);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-int main(int argc, char *argv[]);
-void multiply(char *f, char *s);
-int onlyNumbers(char *c);
-int onlyNumbers(char *c);
-int _strlen(char *s);
-char *_memset(char *s, char b, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int _strlen(char *s);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+
 
 #endif
