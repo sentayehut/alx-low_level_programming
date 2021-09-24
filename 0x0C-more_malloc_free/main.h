7c5a9ffe7d249290048ac604138c5f0cefd20145
sentayehut <tsentayehu4@gmail.com>
2021-09-24 11:07:03 -0700

    mulitply

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -21,6 +21,10 @@ void _puts(char *str);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 int main(int argc, char const *argv[]);
-
+void multiply(char *f, char *s);
+int onlyNumbers(char *c);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
