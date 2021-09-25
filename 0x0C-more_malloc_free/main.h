719c67219e3580f9b80a8ad9cc36cc8e1a44803a
sentayehut <tsentayehu4@gmail.com>
2021-09-24 18:17:59 -0700

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,6 +16,11 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+nt main(int argc, char const *argv[]);
+
 
 
 #endif
