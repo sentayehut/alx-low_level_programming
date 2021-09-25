8e61ef856d4f2ae42eab3295ec3f6a6d1a276e5b
sentayehut <tsentayehu4@gmail.com>
2021-09-24 18:09:05 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,50 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * string_nconcat - concatenate two strings
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string.
+ * @s2: pointer to 2nd string.
+ * @n: Number of bytes from n2 to concatenate.
  *
- * @s1: is a pointer to the first string
- * @s2: is a pointer to the second string
- * @n: an integer (6) with the limit of what we are going to add to s1
- *
- * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
+ * Return:Pointer to space in memory containing concatenated string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	/* unsigned int var because we need to compare with n */
-	char *strings;
-	unsigned int lengths1, lengths2;
+	char *str;
+	unsigned int i, j, s1_length, s2_length;
 
-	/* task condition if NULL passed, should be treated as empty string */
-	/* same condition of project 0x0B 2-str.concatena */
+/*Check if the strings passed are null*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/* I go through both strings so I can find their length */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
+/*Compute the length of the strings*/
+
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+		;
+
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+		;
+/*Memory reservation-for case 1 & 2.*/
+	str = malloc(s1_length + n + 1);
+	if (str == NULL)
 	{
-	}
-	for (lengths2 = 0; s2[lengths2] != '\0'; lengths2++)
-	{
-	}
-	/* Now that I know the length of s2 I can compare it with n */
-	/* and set the task condition */
-	if (n > lengths2)
-		n = lengths2;
-	/* now set the malloc with the lenghts */
-	/* n is now equal to lengths2 */
-	/* plus 1 because we need to save space for the last '\0' of strings */
-	strings = malloc((lengths1 + n + 1) * sizeof(char));
-	if (strings == NULL)
 		return (NULL);
-	/* now we add the content of strings to new strings pointer */
-	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
-		strings[lengths1] = s1[lengths1];
-	/* iterate while lengths2 < n so we print only school */
-	/* lenghts1 keeps counting while we go trough s2 */
-	for (lengths2 = 0; lengths2 < n; lengths2++, lengths1++)
-	strings[lengths1] = s2[lengths2];
-	strings[lengths1] = '\0';
-	return (strings);
+	}
+/*Copy first string into str.*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+/*copy second stringg into str.*/
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+
+	str[i] = '\0';
+	return (str);
 }
