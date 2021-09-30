21b168ff5d11218e8e62c9e9b7b73ff5b1ad056e
sentayehut <tsentayehu4@gmail.com>
2021-09-30 10:26:33 -0700

    varied function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,7 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
-#include <stdarg.h>
 
-/**
- * struct format_handler - struct handles format and there callbacks
- *
- * @format: the format
- * @printer: callback
- */
+#include <stdarg.h>
 
 typedef struct printer
 {
@@ -15,20 +9,10 @@ typedef struct printer
 	void (*print)(va_list arg);
 
 } printer_t;
-typedef struct format_handler
-{
-	char format;
-	void (*printer)(va_list *args_list);
-} fh;
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list *args);
-void print_integer(va_list *args);
-void print_float(va_list *args);
-void print_string(va_list *args);
 
 #endif
