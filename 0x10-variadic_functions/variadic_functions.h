b2994032b09f7f7eff05d8cf5d80de7286a9006e
sentayehut <tsentayehu4@gmail.com>
2021-09-30 09:24:09 -0700

    varidic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
+/**
+ * struct format_handler - struct handles format and there callbacks
+ *
+ * @format: the format
+ * @printer: callback
+ */
+
+typedef struct format_handler
+{
+	char format;
+	void (*printer)(va_list *args_list);
+} fh;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void _printchar(va_list list);
-void _printstr(va_list list);
-void _printfloat(va_list list);
-void _printint(va_list list);
+void print_char(va_list *args);
+void print_integer(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 
-typedef struct checker
-{
-	char *type;
-	void (*f)();
-} checker;
 #endif
