c3ec4d24f69d723d8d7c0bd7e8357cfe3e44a607
sentayehut <tsentayehu4@gmail.com>
2021-09-30 10:43:15 -0700

    my fuchion header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,28 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
-
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
-typedef struct printer
+/**
+ * struct format_handler - struct handles format and there callbacks
+ *
+ * @format: the format
+ * @printer: callback
+ */
+
+typedef struct format_handler
 {
-	char *symbol;
-	void (*print)(va_list arg);
+	char format;
+	void (*printer)(va_list *args_list);
+} fh;
 
-} printer_t;
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list *args);
+void print_integer(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 
 #endif
