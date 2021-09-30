5fd0c682c5a9a30064b0105134b383c1ca502ed1
sentayehut <tsentayehu4@gmail.com>
2021-09-30 10:09:17 -0700

    varidic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,10 +9,19 @@ typedef struct printer
 	void (*print)(va_list arg);
 
 } printer_t;
+typedef struct format_handler
+{
+	char format;
+	void (*printer)(va_list *args_list);
+} fh;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list *args);
+void print_integer(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 
 #endif
