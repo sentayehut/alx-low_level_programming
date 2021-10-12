56f40f529c6595a2ed50d61de3b895409065567f
sentayehut <tsentayehu4@gmail.com>
2021-10-12 06:20:47 -0700

    find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,28 +1,38 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
- * find_listint_loop - finds the loop in a linked list.
- *
- * @head: list head
- *
- * Return: The address of the node where the
- * loop starts, or NULL if there is no loop
+ * find_listint_loop - find the loop in a linked list
+ * @head: head of linked list
+ * Description: Not allowed to use malloc, free or arrays.
+ * Can only declare a max of 2 variables.
+ * Return: Address of node where loop starts, or NULL if no loop found.
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *ptr, *end;
+	listint_t *currents, *currentf;
 
 	if (head == NULL)
 		return (NULL);
 
-	for (end = head->next; end != NULL; end = end->next)
+	currents = currentf = head;
+	do {
+		if (currents->next)
+			currents = currents->next;
+		else
+			return (NULL);
+
+		if (currentf->next->next)
+			currentf = currentf->next->next;
+		else
+			return (NULL);
+	} while (currentf != currents);
+
+	currents = head;
+	while (currentf != currents)
 	{
-		if (end == end->next)
-			return (end);
-		for (ptr = head; ptr != end; ptr = ptr->next)
-			if (ptr == end->next)
-				return (end->next);
+		currentf = currentf->next;
+		currents = currents->next;
 	}
-	return (NULL);
+
+	return (currents);
 }
